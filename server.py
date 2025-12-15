8acd44aba263c8b4a514d2bac80207285f137be4
zxy-88 <saravutc27@gmail.com>
2025-12-15 14:29:08 +0700

    #

--- server.py
+++ server.py
@@ -72,4 +72,4 @@ def fetch_data():
 
 if __name__ == '__main__':
     print("🚀 Server running at http://127.0.0.1:5000")
-    app.run(debug=True, port=5000)
+    app.run(debug=True,host='0.0.0.0', port=5000)
